d9dc5682f0810386685b7fbfa370069ae3de5ed6
denzilferreira <ferreiradenzil@gmail.com>
2017-02-19 16:13:52 +0200

    - Removed aware client source - Override the layouts for ESM - React to the ESMs answered

--- app/src/main/java/ch/usi/inf/mc/awareapp/Database/ESMObserver.java
+++ app/src/main/java/ch/usi/inf/mc/awareapp/Database/ESMObserver.java
@@ -1,24 +1,36 @@
 package ch.usi.inf.mc.awareapp.Database;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.database.ContentObserver;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.net.Uri;
 import android.os.Handler;
+
+import com.aware.ESM;
+import com.aware.providers.ESM_Provider;
+import com.aware.utils.DatabaseHelper;
+import com.aware.utils.WebserviceHelper;
+
+import ch.usi.inf.mc.awareapp.RemoteStorage.ESMUploader;
 
 /**
  * Created by Danilo on 2/18/17.
  */
 
-public class ESMObserver extends ContentObserver{
-
-    public ESMObserver(Handler handler) {
-        super(handler);
+public class ESMObserver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(ESM.ACTION_AWARE_ESM_ANSWERED)) {
+            Cursor latest_answered = context.getContentResolver().query(ESM_Provider.ESM_Data.CONTENT_URI, null, ESM_Provider.ESM_Data.STATUS + "=" + ESM.STATUS_ANSWERED, null, ESM_Provider.ESM_Data.TIMESTAMP + " DESC LIMIT 1");
+            if (latest_answered != null && latest_answered.moveToFirst()) {
+                Intent esmoffload = new Intent(context, ESMUploader.class);
+                esmoffload.putExtra(ESMUploader.EXTRA_ESM_DATA, DatabaseHelper.cursorToString(latest_answered)); //this converts the cursor to a JSON you can sent anywhere.
+                context.startService(esmoffload);
+            }
+            if (latest_answered != null && ! latest_answered.isClosed()) latest_answered.close();
+        }
     }
-
-    public void onChange(boolean selfChange){
-        this.onChange(selfChange, null);
-
-    }
-
-
 }
